e0608c02bc9cd2669b2cf3598b5699476c8dabd5
sanne87 <42010490+sanne87@users.noreply.github.com>
2018-08-08 21:07:52 +0200

    endringer

--- trivia.js
+++ trivia.js
@@ -3,10 +3,11 @@ let userAnswer;
 let rightanswer;
 let score = 0;
 let questnumber = 0;
+let contQuest = 0;
 
 $('.score').hide();
 $('.contribute').hide();
-
+$('.buttons').hide();
 // questions
 
 class task {
@@ -34,7 +35,7 @@ const quest2 = new task ('I hvilket land ligger hovedkontoret til flyselskapet R
 const quest3 = new task ('I hvilken amerikansk delstat ligger Grand Canyon', ['California', 'Texas', 'New York', 'Arizona']);
 
 let questions = [quest1, quest2, quest3];
-let randomNumber = Math.floor(Math.random()*questions.length);
+
 
 //randomise answers + set questions
 function randomanswers(taskan) {
@@ -43,7 +44,10 @@ function randomanswers(taskan) {
   let temporaryValue;
   let randomIndex;
   rightanswer = taska[3];
-  questions.splice(taskan, 1);
+
+  // avoid same question twise by removing it
+  let x = questions.indexOf(taskan);
+  questions.splice(x, 1);
 
   while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random()*currentIndex);
@@ -70,49 +74,68 @@ function randomanswers(taskan) {
     document.getElementById('alt4_').value = taska[3];
 }
 
-function setquestions() {
-
-}
-
 
 //get answer from user + store them + game over
 
 function submit() {
 //store answers
   userAnswer = jQuery('#answers input:radio:checked').val();
-
+  questnumber +=1;
   if (userAnswer === rightanswer) {
   score += 1;
-  questnumber += 1;
-  localStorage.setItem('oppgave '+[questnumber], 'riktig');
 
+  localStorage.setItem('oppgave'+[questnumber], 'riktig');
   }
-
     else {
-      questnumber +=1;
-
-      localStorage.setItem('oppgave '+[questnumber], 'feil');
-
+      localStorage.setItem('oppgave'+[questnumber], 'feil');
     }
-
 //game over?
+
+
     if (questnumber>=3) {
         $('.answers').hide();
         $('.question').hide();
         $('.score').show();
-        document.getElementById('scoretext').innerHTML = 'Du fikk ' + score + ' poeng!';
+        let oppg1 = localStorage.getItem('oppgave1');
+        let oppg2 = localStorage.getItem('oppgave2');
+        let oppg3 = localStorage.getItem('oppgave3');
 
-        $('.contribute').show();
+
+        document.getElementById('scoretext').innerHTML = 'Du fikk ' + score + ' poeng!';
+        document.getElementById('results').innerHTML = `oppgave 1: ${oppg1} <br> oppgave 2: ${oppg2} <br> oppgave 3: ${oppg3} <br> `
+        $('.buttons').show();
     }
 
-    // avoid same question twise by removing it
-
-    console.log(x);
     //keep playing
+    console.log(score);
+    console.log(questnumber);
 
     randomanswers(questions[Math.floor(Math.random()*questions.length)]);
 
+
+
   }
+
+function contributeFields() {
+  $('.contribute').show();
+}
+
+function csubmit() {
+
+  let cquest = document.getElementById('cspm').innerHTML
+  let calt1 = document.getElementById('calt1').innerHTML
+  let calt2 = document.getElementById('calt2').innerHTML
+  let calt3 = document.getElementById('calt3').innerHTML
+  let calt4 = document.getElementById('calt4').innerHTML
+
+
+  var quest[] = new task (cquest, [calt1, calt2, calt3, calt4]);
+  questions.push(quest[variabelttall her]);
+
+  contQuest += 1;
+  console.log(questions);
+
+}
 
 
 randomanswers(questions[Math.floor(Math.random()*questions.length)]);
